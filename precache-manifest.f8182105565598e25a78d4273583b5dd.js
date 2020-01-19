3142ad35cc10ddaca17c336856ed1536db31d6d0
Andrew Persad <akpersad@gmail.com>
2020-01-19 15:34:30 -0500

    Updates

---
+++ precache-manifest.f8182105565598e25a78d4273583b5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae4d8372dd45928378564e7aa8d5653",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "1bd14ed06f6a23fc264e",
+    "url": "/wedding/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b934d377e89cc163fea3",
+    "url": "/wedding/static/css/main.cf4f22fc.chunk.css"
+  },
+  {
+    "revision": "1bd14ed06f6a23fc264e",
+    "url": "/wedding/static/js/2.3d4aa3ce.chunk.js"
+  },
+  {
+    "revision": "59d4dfc8f8cfe664c52461f468e0251d",
+    "url": "/wedding/static/js/2.3d4aa3ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b934d377e89cc163fea3",
+    "url": "/wedding/static/js/main.63897c1a.chunk.js"
+  },
+  {
+    "revision": "b5e62b65536c77ec80a9",
+    "url": "/wedding/static/js/runtime-main.2ebebd9b.js"
+  }
+]);
